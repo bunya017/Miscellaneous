d1a46d4dd2c534910892aafb186c837093dae7d8
Bunyaminu Sani <dollabills007@gmail.com>
2019-03-13 20:54:39 +0100

    Refactor

--- VSCode_to_sublime.py
+++ VSCode_to_sublime.py
@@ -8,25 +8,27 @@ with open(filename, encoding='utf-8') as f_obj:
 	vs_code_snippets = json.load(f_obj)
 
 vs_code_snippets_keys = list(vs_code_snippets.keys())
-snippet_name = 'snippets/%s.sublime-snippet' % (
-	vs_code_snippets[vs_code_snippets_keys[0]]['prefix']
-)
-snippet_content = '''
-<snippet>
-	<content><![CDATA[
-%s
-]]></content>
-	<tabTrigger>%s</tabTrigger>
-	<description>%s</description>
-</snippet>
-''' % (
-	vs_code_snippets[vs_code_snippets_keys[0]]['body'][0],
-	vs_code_snippets[vs_code_snippets_keys[0]]['prefix'],
-	vs_code_snippets[vs_code_snippets_keys[0]]['description']
-)
-if not os.path.exists('snippets'):
-	os.makedirs('quasar_snippets')
-with open(snippet_name, 'w') as w_obj:
-	w_obj.write(snippet_content.strip())
+
+for i in range(len(vs_code_snippets_keys)):
+	snippet_name = 'snippets/%s.sublime-snippet' % (
+		vs_code_snippets[vs_code_snippets_keys[i]]['prefix']
+	)
+	snippet_content = '''
+	<snippet>
+		<content><![CDATA[
+	%s
+	]]></content>
+		<tabTrigger>%s</tabTrigger>
+		<description>%s</description>
+	</snippet>
+	''' % (
+		vs_code_snippets[vs_code_snippets_keys[i]]['body'][0],
+		vs_code_snippets[vs_code_snippets_keys[i]]['prefix'],
+		vs_code_snippets[vs_code_snippets_keys[i]]['description']
+	)
+	if not os.path.exists('snippets'):
+		os.makedirs('snippets')
+	with open(snippet_name, 'w') as w_obj:
+		w_obj.write(snippet_content.strip())
 
 print('Completed successfully')
